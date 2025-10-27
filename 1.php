1d19ab3664e91f6c67b48d1c11da8b6e1dac6404
algoga <algoga@alumnatflorida.es>
2025-10-27 09:12:13 +0100

    "1.php finalizado"

---
+++ 1.php
@@ -0,0 +1,15 @@
+<?php
+ "Tokyo,Mexico City,New York City,Mumbai,Seoul,Shanghai,Lagos,Buenos Aires, Cairo,London";
+$capitales=[
+    ['ciudad'=>'Tokyo'],
+    ['ciudad'=>'Mexico City'],
+    ['ciudad'=>'New York City'],
+    ['ciudad'=>'Mumbai'],
+    ['ciudad'=>'Seoul'],
+    ['ciudad'=>'Shanghai'],
+    ['ciudad'=>'Lagos'],
+    ['ciudad'=>'Buenos Aires'],
+    ['ciudad'=>'Cairo'],
+    ['ciudad'=>'London']
+];
+var_dump($capitales);   
