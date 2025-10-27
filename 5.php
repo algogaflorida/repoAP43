af7bcd592121375220600373049d835c7b23d5d8
algoga <algoga@alumnatflorida.es>
2025-10-27 10:36:33 +0100

    "Modf Ejercicio 5"

--- 5.php
+++ 5.php
@@ -1,4 +1,9 @@
 <?php
 $data = "Tokyo, Japan; Mexico City, Mexico; New York City, USA; Mumbai, India; Seoul, Korea; Shanghai, China; Lagos, Nigeria; Buenos Aires, Argentina; Cairo, Egypt; London, England.";
 $paiscapital=explode(";",$data);
-var_dump($paiscapital);    
+$contador=count($paiscapital);
+$array=[];
+for ($i=0;$i<$contador;$i++){
+    $array[$i]=explode(",", $paiscapital[$i]);
+}
+var_dump($array);    
