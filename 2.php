62caa08aa736dc5142840f01d68b0dabc1bb91a0
algoga <algoga@alumnatflorida.es>
2025-10-27 09:15:41 +0100

    "2.php finalizado"

---
+++ 2.php
@@ -0,0 +1,14 @@
+<?php
+$países=[
+    ['pais'=>'Japón'],
+    ['pais'=>'México'],
+    ['pais'=>'Estados Unidos'],
+    ['pais'=>'India'],
+    ['pais'=>'Corea'],
+    ['pais'=>'China'],
+    ['pais'=>'Nigeria'],
+    ['pais'=>'Argentina'],
+    ['pais'=>'Egipto'],
+    ['pais'=>'Inglaterra']
+];
+var_dump($países);
